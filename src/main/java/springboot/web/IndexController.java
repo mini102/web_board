e3173faa015bbaa6b911ea6a0b77f7ef6d054457
mini102 <mooya1024@ajou.ac.kr>
2022-06-19 00:47:24 +0900

    게시판 조회/등록/삭제 기능 구현

---
+++ src/main/java/springboot/web/IndexController.java
@@ -0,0 +1,42 @@
+package springboot.web;
+
+//import springboot.config.auth.LoginUser;
+//import springboot.config.auth.dto.SessionUser;
+import springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import springboot.web.dto.PostsResponseDto;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+//    @GetMapping("/")
+//    public String index(){
+//        return "index";
+//    }
+
+    @GetMapping("/")
+    public String index(Model model) {  //Model: 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+        model.addAttribute("posts", postsService.findAllDesc());  //postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -28,7 +28,7 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 등록되었습니다.');
-            window.location.href = '/';
+            window.location.href = '/';  //글 등록이 성공하면 메인페이지(/)로 이동
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
@@ -54,20 +54,20 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
-    delete : function () {
-        var id = $('#id').val();
+   delete : function () {
+       var id = $('#id').val();
 
-        $.ajax({
-            type: 'DELETE',
-            url: '/api/v1/posts/'+id,
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8'
-        }).done(function() {
-            alert('글이 삭제되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
+       $.ajax({
+           type: 'DELETE',
+           url: '/api/v1/posts/'+id,
+           dataType: 'json',
+           contentType:'application/json; charset=utf-8'
+       }).done(function() {
+           alert('글이 삭제되었습니다.');
+           window.location.href = '/';
+       }).fail(function (error) {
+           alert(JSON.stringify(error));
+       });
     }
 
 };
